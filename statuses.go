e6f1cea049eba1c9920a9200fe58f520255eadf3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-27 01:10:13 -0700

    initial commits

--- statuses.go
+++ statuses.go
@@ -1,5 +1,9 @@
 package hg
 
+import (
+	"io"
+)
+
 const (
 	StatusInput              = 10
 	StatusSensitiveInput     = 11
@@ -7,7 +11,7 @@ const (
 	StatusSuccess            = 20
 
 	StatusRedirectTemporary  = 30
-	StatusRedirecPermanent   = 31
+	StatusRedirectPermanent  = 31
 
 	StatusTemporaryFailure   = 40
 	StatusServerUnavailable  = 41
@@ -21,3 +25,171 @@ const (
 	StatusProxyRequestFailed = 53
 	StatusBadRequest         = 59
 )
+
+// 10
+func Input(w ResponseWriter, meta string) {
+	const statuscode = StatusInput
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 11
+func SensitiveInput(w ResponseWriter, meta string) {
+	const statuscode = StatusSensitiveInput
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 30
+func RedirectTemporary(w ResponseWriter, meta string) {
+	const statuscode = StatusRedirectTemporary
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 31
+func RedirectPermanent(w ResponseWriter, meta string) {
+	const statuscode = StatusRedirectPermanent
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 40
+func TemporaryFailure(w ResponseWriter, meta string) {
+	const statuscode = StatusTemporaryFailure
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 41
+func ServerUnavailable(w ResponseWriter, meta string) {
+	const statuscode = StatusServerUnavailable
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 42
+func CGIError(w ResponseWriter, meta string) {
+	const statuscode = StatusCGIError
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 43
+func ProxyError(w ResponseWriter, meta string) {
+	const statuscode = StatusProxyError
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 44
+func SlowDown(w ResponseWriter, meta string) {
+	const statuscode = StatusSlowDown
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 50
+func PermanentFailture(w ResponseWriter, meta string) {
+	const statuscode = StatusPermanentFailture
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 51
+func NotFound(w ResponseWriter, meta string) {
+	const statuscode = StatusNotFound
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 52
+func Gone(w ResponseWriter, meta string) {
+	const statuscode = StatusGone
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 53
+func ProxyRequestFailed(w ResponseWriter, meta string) {
+	const statuscode = StatusProxyRequestFailed
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
+
+// 59
+func BadRequest(w ResponseWriter, meta string) {
+	const statuscode = StatusBadRequest
+
+	if nil == w {
+		return
+	}
+
+	io.WriteString(w.Meta(), meta)
+	w.WriteHeader(statuscode)
+}
