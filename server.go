36a6b1aece4216925af03bb50bd45637068e75a2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-27 21:42:41 -0700

    initial commits

--- server.go
+++ server.go
@@ -4,11 +4,110 @@ import (
 	"net"
 )
 
+// ListenAndServe listens on the TCP network address `addr` and then spawns a call to the ServeMercuary
+// method on the `handler` to serve each incoming connection.
+//
+// For a very simple example:
+//
+//	package main
+//	
+//	import (
+//		"github.com/reiver/go-hg"
+//	)
+//	
+//	func main() {
+//	
+//		//@TODO: In your code, you would probably want to use a different handler.
+//		var handler hg.Handler = hg.DebugHandler
+//	
+//		err := hg.ListenAndServe(":1961", handler)
+//		if nil != err {
+//			//@TODO: Handle this error better.
+//			panic(err)
+//		}
+//	}
+func ListenAndServe(addr string, handler Handler) error {
+	server := &Server{Addr: addr, Handler: handler}
+	return server.ListenAndServe()
+}
+
+// Serve accepts an incoming Mercuary Protocol client connection on the net.Listener `listener`.
+func Serve(listener net.Listener, handler Handler) error {
+
+	server := &Server{Handler: handler}
+	return server.Serve(listener)
+}
+
 // Server is a Mercury Protocol server.
+//
+// For a simple example:
+//
+//	package main
+//	
+//	import (
+//		"github.com/reiver/go-hg"
+//	)
+//	
+//	func main() {
+//	
+//		var handler hg.Handler = hg.DebugHandler
+//	
+//		server := &hg.Server{
+//			Addr:":1961",
+//			Handler:handler,
+//		}
+//	
+//		err := server.ListenAndServe()
+//		if nil != err {
+//			//@TODO: Handle this error better.
+//			panic(err)
+//		}
+//	}
 type Server struct {
 	Addr string // TCP address to listen on; if empty defaults to ":1961"
 	Handler Handler // handler to invoke; if nil defaults to hg.DebugServer
 	Logger Logger
+}
+
+// ListenAndServe listens on the TCP network address 'server.Addr' and then spawns a call to the ServeMercury
+// method on the 'server.Handler' to serve each incoming connection.
+//
+// For a simple example:
+//
+//	package main
+//	
+//	import (
+//		"github.com/reiver/go-hg"
+//	)
+//	
+//	func main() {
+//	
+//		var handler hg.Handler = hg.EchoHandler
+//	
+//		server := &telnet.Server{
+//			Addr:":1961",
+//			Handler:handler,
+//		}
+//	
+//		err := server.ListenAndServe()
+//		if nil != err {
+//			//@TODO: Handle this error better.
+//			panic(err)
+//		}
+//	}
+func (server *Server) ListenAndServe() error {
+
+	addr := server.Addr
+	if "" == addr {
+		addr = ":1961"
+	}
+
+	listener, err := net.Listen("tcp", addr)
+	if nil != err {
+		return err
+	}
+
+	return server.Serve(listener)
 }
 
 // Serve accepts an incoming Mercury Protocol client connection on the net.Listener ‘listener’.
