44ef2667c49e1c73a42f676e6b0d268044b1a374
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-27 21:32:29 -0700

    initial commits

--- errors.go
+++ errors.go
@@ -5,10 +5,12 @@ import (
 )
 
 var (
-	errExpectedLineFeed = errors.New("hg: expected line-feed character")
-	errNilReceiver = errors.New("hg: nil receiver")
-	errNilWriter   = errors.New("hg: nil io.Writer")
-	errNilSource   = errors.New("hg: nil source")
-	errNothing     = errors.New("hg: nothing")
-	errRuneError   = errors.New("hg: rune error")
+	errExpectedLineFeed        = errors.New("hg: expected line-feed character")
+	errHeaderAlreadyWritten    = errors.New("hg: header already written")
+	errHeaderNotAlreadyWritten = errors.New("hg: header not not written")
+	errNilReceiver             = errors.New("hg: nil receiver")
+	errNilWriter               = errors.New("hg: nil io.Writer")
+	errNilSource               = errors.New("hg: nil source")
+	errNothing                 = errors.New("hg: nothing")
+	errRuneError               = errors.New("hg: rune error")
 )
