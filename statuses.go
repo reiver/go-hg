914a18de13f50f364863b09b068e2bcea9396cf4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-28 23:09:01 -0700

    initial commits

--- statuses.go
+++ statuses.go
@@ -10,19 +10,19 @@ package hg
 //		w.WriteHeader(hg.StatusNotFound, "uh oh!")
 //	}
 const (
-	StatusInput              = 10
-	StatusSensitiveInput     = 11
+	StatusInput               = 10
+	StatusSensitiveInput      = 11
 
-	StatusSuccess            = 20
+	StatusSuccess             = 20
 
-	StatusRedirectTemporary  = 30
-	StatusRedirectPermanent  = 31
+	StatusRedirectTemporary   = 30
+	StatusRedirectPermanent   = 31
 
-	StatusTemporaryFailure   = 40
-	StatusServerUnavailable  = 41
-	StatusCGIError           = 42
-	StatusProxyError         = 43
-	StatusSlowDown           = 44
+	StatusTemporaryFailure    = 40
+	StatusServerUnavailable   = 41
+	StatusCGIError            = 42
+	StatusProxyError          = 43
+	StatusSlowDown            = 44
 
 	StatusPermanentFailure    = 50
 	StatusNotFound            = 51
