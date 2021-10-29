940c6420321c0f2783fdf52f17a21220d323df16
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-28 22:35:14 -0700

    initial commits

--- errors.go
+++ errors.go
@@ -5,8 +5,12 @@ import (
 )
 
 var (
+	errBadResponse             = errors.New("hg: bad response")
 	errExpectedLineFeed        = errors.New("hg: expected line-feed character")
+	errHeaderAlreadyRead       = errors.New("hg: header already read")
 	errHeaderAlreadyWritten    = errors.New("hg: header already written")
+	errNilCaller               = errors.New("hg: nil caller")
+	errNilReader               = errors.New("hg: nil reader")
 	errNilReceiver             = errors.New("hg: nil receiver")
 	errNilWriter               = errors.New("hg: nil io.Writer")
 	errNilSource               = errors.New("hg: nil source")
