f7f813ee3a4c4a1744d089e02abf6c03ddf1dad3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-28 19:28:31 -0700

    initial commits

--- handler.go
+++ handler.go
@@ -1,6 +1,6 @@
 package hg
 
-// Handler represents someething that responds to an Mercury request.
+// Handler represents someething that responds to an Mercury Protocol request.
 type Handler interface {
 	ServeMercury(w ResponseWriter, r Request)
 }
