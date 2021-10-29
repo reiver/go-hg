4e2567f90f40f71d59b41c9e81d07b3af3943bec
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-28 22:34:53 -0700

    initial commits

--- statuscodefromrunes_test.go
+++ statuscodefromrunes_test.go
@@ -12,6 +12,65 @@ func TestStatuscodeFromRunes_success(t *testing.T) {
 		Expected int
 	}{
 		{
+			MostSignificant: '0',
+			LeastSignificant: '0',
+			Expected:          0,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '1',
+			Expected:          1,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '2',
+			Expected:          2,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '3',
+			Expected:          3,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '4',
+			Expected:          4,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '5',
+			Expected:          5,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '6',
+			Expected:          6,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '7',
+			Expected:          7,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '8',
+			Expected:          8,
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '9',
+			Expected:          9,
+		},
+
+
+
+
+
+
+
+
+
+		{
 			MostSignificant: '1',
 			LeastSignificant: '0',
 			Expected:         10,
@@ -117,6 +176,70 @@ func TestStatuscodeFromRunes_success(t *testing.T) {
 			LeastSignificant: '2',
 			Expected:         62,
 		},
+
+
+
+
+
+
+
+
+
+		{
+			MostSignificant: '8',
+			LeastSignificant: '9',
+			Expected:         89,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '0',
+			Expected:         90,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '1',
+			Expected:         91,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '2',
+			Expected:         92,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '3',
+			Expected:         93,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '4',
+			Expected:         94,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '5',
+			Expected:         95,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '6',
+			Expected:         96,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '7',
+			Expected:         97,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '8',
+			Expected:         98,
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: '9',
+			Expected:         99,
+		},
 	}
 
 	for testNumber, test := range tests {
@@ -140,4 +263,279 @@ func TestStatuscodeFromRunes_success(t *testing.T) {
 		}
 	}
 }
-v
+
+func TestStatuscodeFromRunes_failure(t *testing.T) {
+
+	tests := []struct{
+		MostSignificant  rune
+		LeastSignificant rune
+	}{
+		{
+			MostSignificant: 0,
+			LeastSignificant: 0,
+		},
+
+
+
+		{
+			MostSignificant: 0,
+			LeastSignificant: 0,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 1,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 2,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 3,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 4,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 5,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 6,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 7,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 8,
+		},
+		{
+			MostSignificant: 0,
+			LeastSignificant: 9,
+		},
+
+
+
+		{
+			MostSignificant: 1,
+			LeastSignificant: 0,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 1,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 2,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 3,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 4,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 5,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 6,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 7,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 8,
+		},
+		{
+			MostSignificant: 1,
+			LeastSignificant: 9,
+		},
+
+
+
+		{
+			MostSignificant: 2,
+			LeastSignificant: 0,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 1,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 2,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 3,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 4,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 5,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 6,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 7,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 8,
+		},
+		{
+			MostSignificant: 2,
+			LeastSignificant: 9,
+		},
+
+
+
+		{
+			MostSignificant: 3,
+			LeastSignificant: 0,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 1,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 2,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 3,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 4,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 5,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 6,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 7,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 8,
+		},
+		{
+			MostSignificant: 3,
+			LeastSignificant: 9,
+		},
+
+
+
+		{
+			MostSignificant: 9,
+			LeastSignificant: 0,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 1,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 2,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 3,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 4,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 5,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 6,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 7,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 8,
+		},
+		{
+			MostSignificant: 9,
+			LeastSignificant: 9,
+		},
+
+
+
+		{
+			MostSignificant: '/',
+			LeastSignificant: '0',
+		},
+		{
+			MostSignificant: '0',
+			LeastSignificant: '/',
+		},
+
+
+
+		{
+			MostSignificant: ':',
+			LeastSignificant: '9',
+		},
+		{
+			MostSignificant: '9',
+			LeastSignificant: ':',
+		},
+
+
+
+		{
+			MostSignificant: 'a',
+			LeastSignificant: 'b',
+		},
+
+
+
+		{
+			MostSignificant: 0x7F,
+			LeastSignificant: 0x7F,
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		actual, valid := statuscodeFromRunes(test.MostSignificant, test.LeastSignificant)
+		if valid {
+			t.Errorf("For test #%d, did not expect result to valid", testNumber)
+			t.Logf(" MOST-SIGNIFICANT: %q", string(test.MostSignificant))
+			t.Logf("LEAST-SIGNIFICANT: %q", string(test.LeastSignificant))
+			t.Logf("ACTUAL: %d", actual)
+			continue
+		}
+	}
+}
