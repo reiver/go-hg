8dd60c98b782c55be8b03c0f9b6d4a0b330eca2c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-31 11:05:07 -0700

    initial commits

--- handler.go
+++ handler.go
@@ -1,6 +1,9 @@
 package hg
 
-// Handler represents someething that responds to an Mercury Protocol request.
+// Handler represents something that responds to an Mercury Protocol request.
+//
+// Typically, someone who wants to create a custom Mercury Protocol server would create a type that fits this Handler interface,
+// and then (directly or indirectly) pass it to the Serve to ListenAndServe functions.
 type Handler interface {
 	ServeMercury(w ResponseWriter, r Request)
 }
