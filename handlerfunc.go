e2cabd79e72ca905e5c9a148bfdd73f0dc28cc54
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-31 17:56:29 -0700

    initial commits

--- handlerfunc.go
+++ handlerfunc.go
@@ -1,6 +1,16 @@
 package hg
 
 // HandlerFunc is an adapter that allows one to turn a function into a Handler if the function has the same signature as ServeMercury.
+//
+// For example:
+//
+//	func doIt(w hg.ResponseWriter, r hg.Request) {
+//		// ...
+//	}
+//
+//	// ...
+//
+//	var handler hg.Handler = hg.HandlerFunc(doIt)
 type HandlerFunc func(ResponseWriter, Request)
 
 func (fn HandlerFunc) ServeMercury(w ResponseWriter, r Request) {
